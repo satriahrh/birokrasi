a676dcc4eed2610211699f8dffdb86253c94cc69
Satria H R Harsono <12129322+satriahrh@users.noreply.github.com>
2020-01-24 22:50:18 +0700

    Show Last 20 Articles on Homepage (#10)
    * import graphql from gatsby
    
    * add limit on homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { graphql } from 'gatsby'
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
@@ -31,7 +32,7 @@ export const pageQuery = graphql`
         description
       }
     }
-    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }, limit: 20) {
       edges {
         node {
           excerpt
